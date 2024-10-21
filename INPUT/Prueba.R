b35f4841437258c7e07ec276e0c60734d0013f58
SILVIA TOBES <stg1004@alu.ubu.es>
2024-10-21 14:09:55 +0200

    Correcciónl commit anterior

---
+++ INPUT/Prueba.R
@@ -0,0 +1,10 @@
+# ESTRUCTURACIÓN DE LOS DATOS DE HORAS DE SOL EN JSON
+
+install.packages(rjson)
+install.packages(tidyjson)
+library(rjson)
+library(tidyjson)
+library (tidyverse)
+
+Datos_horas_sol <- fromJSON (file= )
+#cambiar los nombres y establecer una mejor ruta
