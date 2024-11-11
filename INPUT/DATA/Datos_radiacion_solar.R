dc56d4ccbef9c751ef7cdde14597be59a40392fe
SILVIA <stg1004@alu.ubu.es>
2024-11-11 13:04:59 +0100

    Radiación actualización datos

--- INPUT/DATA/Datos_radiacion_solar.R
+++ INPUT/DATA/Datos_radiacion_solar.R
@@ -1,18 +1,14 @@
-
-install.packages("readr")
 library(readr)
 
 # Paso 1: Definir la ruta del archivo
-archivo_txt <- "radiacion_global.txt"
+archivo_txt <- "INPUT/DATA/radiacion_global_txt.txt"
 
 # Paso 2: Leer el archivo como un CSV
-datos <- read.csv(archivo_txt, header = TRUE, sep = ";")
+radiacion_global <- read.csv(archivo_txt, header = TRUE, sep = ";")
 
-# Paso 3: Verifica los datos
-print(datos)
-
-# Leer el archivo
-datos <- read_delim(archivo_txt, delim = ";", col_names = TRUE)
+radiacion_global <- read_delim(file = "INPUT/DATA/radiacion_global_txt",delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 # Mostrar los datos
 print(datos)
+
+
