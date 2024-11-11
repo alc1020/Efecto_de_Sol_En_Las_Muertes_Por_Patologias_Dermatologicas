1099bfa5b613feec9cda23b8eba208f42b380fc7
alc1020 <alc1020@alu.ubu.es>
2024-11-11 10:10:21 +0100

    Chequeo final

--- Datos_Defuncion.R
+++ Datos_Defuncion.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(rjson)
 library(tidyjson)
 
-dato_pato <- fromJSON(file = "INPUT/datos anna/defuncion_patologiasdepiel.json")
+dato_pato <- fromJSON(file = "INPUT/DATA/defuncion_patologiasdepiel.json")
 head(dato_pato)
 
 Datos_Patologia <- spread_all(dato_pato)
