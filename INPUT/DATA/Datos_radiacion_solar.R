feee9191a2689338705dee4df6883e33fea3b08d
SILVIA <stg1004@alu.ubu.es>
2024-11-11 13:09:27 +0100

    Problems

--- INPUT/DATA/Datos_radiacion_solar.R
+++ INPUT/DATA/Datos_radiacion_solar.R
@@ -1,14 +1,3 @@
-library(readr)
-
-# Paso 1: Definir la ruta del archivo
-archivo_txt <- "INPUT/DATA/radiacion_global_txt.txt"
-
-# Paso 2: Leer el archivo como un CSV
-radiacion_global <- read.csv(archivo_txt, header = TRUE, sep = ";")
-
-radiacion_global <- read_delim(file = "INPUT/DATA/radiacion_global_txt",delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
-# Mostrar los datos
-print(datos)
+# LOS DATOS NO SON VÁLIDOS
 
 
