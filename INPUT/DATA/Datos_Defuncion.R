80649ecce696de22dde47b0f405589bb43cc4ba2
alc1020 <alc1020@alu.ubu.es>
2024-11-28 14:07:09 +0100

    Copia seguridad

--- INPUT/DATA/Datos_Defuncion.R
+++ INPUT/DATA/Datos_Defuncion.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(rjson)
 library(tidyjson)
+library(readr)
 
 dato_pato <- fromJSON(file = "INPUT/DATA/defuncion_patologiasdepiel.json")
 head(dato_pato)
@@ -8,9 +9,5 @@ head(dato_pato)
 Datos_Patologia <- spread_all(dato_pato)
 
 view(Datos_Patologia)
-
-
-
-
 
 
